e10eb3ef7637f9447c8afca4f6d2884648e8cba0
Mackenzie Ortiz <mlubben1@gmail.com>
2023-12-15 21:03:45 -0500

    Add files via upload

---
+++ week-3/week-3.js
@@ -0,0 +1,23 @@
+// step 1. fetch the value from the input
+
+function convert() {
+   var fahrenheit = document.getElementById('fahrenheit').value;
+    console.log(value);
+   
+   
+
+}
+
+// step 2. convert it to celsius
+
+function convert() {
+    var fahrenheit = document.getElementById('fahrenheit').value;
+    var celsius = ((parseFloat(fahrenheit) - 32) / 1.8).toFixed(2);
+    console.log(celsius);
+    
+    
+ 
+ }
+
+let txtFahrenheit = "26.67";
+document.getElementById("form-results-example").innerHTML = "Conversion Results"
