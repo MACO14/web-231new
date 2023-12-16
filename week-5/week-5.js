42d38b2d65ff40e3a98f22175fe73776aeb63650
Mackenzie Ortiz <mlubben1@gmail.com>
2023-12-15 21:20:26 -0500

    Add files via upload

---
+++ week-5/week-5.js
@@ -0,0 +1,52 @@
+// Sequence
+// Fibonacci numbers that are less than 40
+
+function pressed(){
+    let choice = document.getElementById("sequence").value;
+    if (choice = num1) {
+        result = "0, 1, 1, 2, 3, 5, 8, 13, 21, 34";
+    }
+    
+    if (choice = num2) {
+        result = "2, 4, 6, 8, 10, 12, 14, 16, 18";
+    }
+    
+    if (choice = next) {
+        result = "1, 3, 5, 7, 9, 11, 13, 15, 17, 19";
+    }
+}
+
+
+const num1 = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+// fibonacci number
+
+while(num1 < 34){
+    console.log('Fibonacci numbers are displayed');
+
+    num1++;
+}
+
+const num2 = [2, 4, 6, 8, 10, 12, 14, 16, 18]
+// even number
+
+while(num2 < 20){
+    console.log('Even numbers are displayed');
+
+    num1++;
+}
+
+const next = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+// odd number
+
+while(next < 20){
+    console.log('Odd numbers are displayed');
+
+    num1++;
+}
+
+function showAlert() {
+    let select = "Invalid selection, please try again!"
+}
+
+
+
